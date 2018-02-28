e4e09854e63827ca5fb988fd49811773986d497b
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-28 16:23:42 +0530

    Create program48.c

---
+++ program48.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,a[n],i,sum=0,avg;
+printf("\nenter the number");
+scanf("%d",&n);
+printf("\nenter the elements");
+for(i=0;i,n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum+=a[i];
+}
+avg=sum/n;
+printf("\naverage integer:%d",avg);
+return 0;
+}
